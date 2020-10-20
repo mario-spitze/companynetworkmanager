8a882d6f01905c0fbbf2d8d753cd1e67f750db19
Mario Reichel <mario@mario.local>
2020-10-20 05:56:37 +0200

    Correct settings template dir

--- django/project/companymanager/settings.py
+++ django/project/companymanager/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'companymanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        ''DIRS': [str(os.path.join(BASE_DIR, 'templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
